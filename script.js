d4f71039673463379281f6f1ecb50dfbf0f89797
William Lucas <williamsantos1506@gmail.com>
2022-08-01 20:52:36 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+const place = document.querySelector("#place");
+const degrees = document.querySelector("#degrees");
+const img = document.querySelector("img");
+const wind = document.querySelector("#wind");
+const content = document.querySelector(".content");
+
+button.addEventListener("click", () => {
+  if (!input.value) return;
+
+  getDataApi();
+});
+
+async function getDataApi() {
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
+    input.value
+  )}&units=metric&appid=4200130a0ec561385430b3e2a57030f4`;
+
+  try {
+    await fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data?.cod && data.cod === "404") {
+          return alert("Local não encontrado!");
+        }
+
+        loadData(data);
+      });
+  } catch (error) {
+    alert(error);
+  }
+}
+
+function loadData(data) {
+  place.innerHTML = `${data.name}, ${data.sys.country}`;
+  degrees.innerHTML = `Temperatura: ${Math.floor(data.main.temp)}° C`;
+  img.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  wind.innerHTML = `Vento: ${data.wind.speed} km/h`;
+  content.style.display = "flex";
+}
